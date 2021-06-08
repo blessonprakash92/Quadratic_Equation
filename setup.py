0564fc1de62b7d27839c914a87a1e8bba612d9a0
BLESSON PRAKASH <blesson91@yahoo.co.in>
2021-06-08 18:09:59 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'BQuadratic_Equation',
+  packages = ['Quadratic_Equation'],
+  version = '0.1',
+  license='MIT',
+  description = 'The library solves quadratic quation and return the zeroes',
+  author = 'BLESSON PRAKASH',
+  author_email = 'blesson.prakash.dokt@pw.edu.pl',
+  url = 'https://github.com/blessonprakash92',
+  download_url = '',
+  keywords = ['Coefficients', 'Zeroes'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: MIT License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
